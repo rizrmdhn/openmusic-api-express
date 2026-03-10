58e0cacde72271737263223276636fab66b4e4f8
Noor Rizki Ramadhan <rizrmdhn.work@gmail.com>
2026-03-10 13:25:31 +0800

    chore(user): remove user module and related files
    - delete user module files including controller, service, repository, routes, schema, and relations
    - update main router to remove user routes

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,7 @@
 import { Router, type Router as ExpressRouter } from 'express';
-import userRoutes from '@/modules/user/user.routes';
 import albumRoutes from '@/modules/album/album.routes';
 import songRoutes from '@/modules/song/song.routes';
 
 export const router: ExpressRouter = Router();
-router.use('/users', userRoutes);
 router.use('/albums', albumRoutes);
 router.use('/songs', songRoutes);
