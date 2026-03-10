e57d53abb5604869fa08748b4f448741a0f843fd
Noor Rizki Ramadhan <rizrmdhn.work@gmail.com>
2026-03-10 13:26:07 +0800

    chore(db): remove user relations and schema exports
    - removed user relations export from relations.ts
    - removed user table export from schema.ts

--- src/db/relations.ts
+++ src/db/relations.ts
@@ -1,3 +1,2 @@
 export * from '@/modules/album/album.relations';
 export * from '@/modules/song/song.relations';
-export * from '@/modules/user/user.relations';
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,3 +1,2 @@
 export * from '@/modules/album/album.table';
 export * from '@/modules/song/song.table';
-export * from '@/modules/user/user.table';
